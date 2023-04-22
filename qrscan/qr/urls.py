d00eb046fde7ca34f1d05c12ab373ab6844c7ab1
Javier <ramosjavier276@gmail.com>
2023-04-21 22:18:58 -0300

    Se implementan los forms

--- qrscan/qr/urls.py
+++ qrscan/qr/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, logging
+from .views import index, logging, qrnuevo
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/login/bienvenido/', index, name='welcome'),
     path('accounts/logout/home/', logging, name='goodbye'),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('qrnuevo/', qrnuevo)
 ]
--- qrscan/qr/views.py
+++ qrscan/qr/views.py
@@ -1,6 +1,7 @@
 from .models import Qr
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import QrForm
 
 @login_required
 def index(request):
@@ -14,3 +15,17 @@ def logging(request):
 
 def usuarios(request):
     return render(request,"registration/login.html")
+
+@login_required
+def qrnuevo(request):
+    form = QrForm()
+    if request.method == 'POST':
+        print(request.POST)
+        form = QrForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        print(form.errors)
+        print("Es un post")
+    return render(request,'qrnuevo.html', {'form':form})
+
