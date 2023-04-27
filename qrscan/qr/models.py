c7366de91c9719082466c730adfd3d375e4c28ba
Javier <ramosjavier276@gmail.com>
2023-04-26 21:18:12 -0300

    Se agregan implementaciones al usuario

--- qrscan/qr/models.py
+++ qrscan/qr/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 import qrcode
 from django_resized import ResizedImageField
+from django.contrib.auth.models import User
 
 
 class Qr(models.Model):
     data = ResizedImageField(size=[200, 200])
     titulo = models.CharField(max_length=100, null=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
--- qrscan/qr/views.py
+++ qrscan/qr/views.py
@@ -5,10 +5,9 @@ from .forms import QrForm
 
 @login_required
 def index(request):
-    qr = Qr.objects.all()
+    qr = Qr.objects.filter(usuario=request.user)
     context = {"qr":qr}
-    return render(request, "index.html", context)
-
+    return render(request,"index.html", context)
 
 def logging(request):
     return render(request,"home.html")
@@ -20,15 +19,10 @@ def usuarios(request):
 def qrnuevo(request):
     form = QrForm()
     if request.method == 'POST':
-        print(request.POST)
         form = QrForm(request.POST,request.FILES)
         if form.is_valid():
-            form.save()
+            instance = form.save(commit=False) #commit= False lo que hace es decir "no guardes esto aun", vamos a aplicar una accion mas y luego si vamos a guardar el post
+            instance.usuario = request.user #Aca lo que hacemos es pedir el USUARIO asociado al modelo del qr a traves del request que esta haciendo el usuario en el form
+            instance.save()
             return redirect('/accounts/login/bienvenido/')
-        print("Es un post")
     return render(request,'qrnuevo.html', {'form':form})
-
-# def photo_view(request):
-#     qr = Qr.objects.all().first()
-#     context = {'photos': qr}
-#     return render(request,'index.html', context)
